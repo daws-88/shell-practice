ca04840c10a65978611569a17b3f8c62ebdec649
Venkat <psai96526@gmail.com>
2026-01-26 12:56:57 +0530

    strated

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "ramesh:: hi suresh , how are you"
+echo "suresh::hello ramesh , i am fine . how are you"
+echo "ramesh::what are you learning now?"
+echo "suresh:: i am learning devops"
