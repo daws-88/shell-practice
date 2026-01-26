493c44ecf2fd9ff5abdf75361cdcc1e4c0b32321
Venkat <psai96526@gmail.com>
2026-01-26 13:06:18 +0530

    strated

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+PERSON1=SAI
+PERSON2=r=RAMU
 
-
-echo "ramesh:: hi suresh , how are you"
-echo "suresh::hello ramesh , i am fine . how are you"
-echo "ramesh::what are you learning now?"
-echo "suresh:: i am learning devops"
+echo $PERSON1:: hi $PERSON2 , how are you"
+echo "suresh::hello $PERSON1 , i am fine . how are you"
+echo "$PERSON1::what are you learning now?"
+echo "$PERSON2:: i am learning devops"
