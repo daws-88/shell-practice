657d48749826607c48476aa29728b82b7d1b9346
Venkat <psai96526@gmail.com>
2026-01-26 19:15:43 +0530

    new

---
+++ 03-varaible.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# is a comment
+
+echo "shiva:: how are you rohith?"
+echo "rohith:: i am fine shiva."
+echo "shiva:: what are u doing rohith?"
+echo "rohith::i am a devops engineer shiva."
