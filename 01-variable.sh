7af959da906f8137cf43e2564357a56fda333e6b
Venkat <psai96526@gmail.com>
2026-01-27 21:34:11 +0530

    new

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PERSON1=$!
+PERSON1=$1
 PERSON2=$2
-echo "$PERSON1:: how are u kohli?"
+echo "$PERSON1:: how are u $PERSON2?"
 echo "$PERSON2:: fine bro."
 echo "$PERSON1:: let's play a t20 cricket."
 echo "$PERSON2:: why not we play."
--- 09-datatype.sh
+++ 09-datatype.sh
@@ -2,5 +2,6 @@
 
 NUMBER1=200
 NUMBER2=500
+NAME=devops
 SUM=$(($NUMBER1+$NUMBER2))
 echo "SUM is: ${SUM}"
