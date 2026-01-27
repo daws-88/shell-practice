3b0107b43194ed60ea2ed9c7dc8737aa7dfbb9c5
Venkat <psai96526@gmail.com>
2026-01-27 14:51:22 +0530

    new

--- 08-special.sh
+++ 08-special.sh
@@ -5,3 +5,7 @@ echo "all variables passed to script: $*"
 echo "script name: $0"
 echo "curent dirctory: $PWD"
 echo "Who is running: $USER"
+echo "home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PIDof this last command in background: $!"
