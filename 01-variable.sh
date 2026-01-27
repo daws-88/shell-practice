f630b8a4d5231aa5db1fa53c86781b1cd3002fdc
Venkat <psai96526@gmail.com>
2026-01-27 21:30:06 +0530

    new

---
+++ 01-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$!
+PERSON2=$2
+echo "$PERSON1:: how are u kohli?"
+echo "$PERSON2:: fine bro."
+echo "$PERSON1:: let's play a t20 cricket."
+echo "$PERSON2:: why not we play."
