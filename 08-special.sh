503aaffebf06e6469e05d4fe6ef1fe7eab370bae
Venkat <psai96526@gmail.com>
2026-01-27 14:47:13 +0530

    new1

---
+++ 08-special.sh
@@ -0,0 +1,7 @@
+#1/bin/bash
+
+echo "all variables passes to script: $@"
+echo "all variables passed to script: $*"
+echo "script name: $0"
+echo "curent dirctory: $PWD"
+echo "Who is running: $USER"
