328c476bf86fc5a29b9d9a398b0a84876ebdd737
Venkat <psai96526@gmail.com>
2026-01-28 10:35:31 +0530

    remove

--- 01-variable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PERSON1=$1
-PERSON2=$2
-echo "$PERSON1:: how are u $PERSON2?"
-echo "$PERSON2:: fine bro."
-echo "$PERSON1:: let's play a t20 cricket."
-echo "$PERSON2:: why not we play."
--- 07-variable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NUMBER1=500
-NUMBER2=200
-NAME=devops
-SUM=$(($NUMBER1+$NUMBER2))
-echo "SUM is: $SUM"
-NAME=("linux" "aws" "docker")
-echo "script name is: ${NAME[2]}"
