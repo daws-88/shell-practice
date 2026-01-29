3a75b1f3616e93b8c3dc16d277241adec2c86d46
Venkat <psai96526@gmail.com>
2026-01-29 11:53:47 +0530

    new

--- 07-variable.sh
+++ 07-variable.sh
@@ -4,5 +4,5 @@ START_TIME=$(date +%s)
 sleep 25
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-START_TIME))
-echo "script executed in: $TOTAL_TTIME in sec"
+echo "script executed in: $TOTAL_TTIME  seconds"
 
