be1ee94969767815ecb2c9a3addeffe4c889e2ef
Venkat <psai96526@gmail.com>
2026-01-29 11:06:28 +0530

    mow

---
+++ 03-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+# this arguments passing
+PERSON1=$1
+PERSON2=$2
+echo "dhoni:: how are you rohith ?"
+echo "raina:: i am fine dhoni."
+echo "dhoni:: shall we play crickeit ?"
+echo "raain:: yes we paly"
