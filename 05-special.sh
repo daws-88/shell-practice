c045565820d250dcd4bcae317259643820a68246
Venkat <psai96526@gmail.com>
2026-01-29 12:43:36 +0530

    up

---
+++ 05-special.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#v special variables.
+echo "All variables passed to script: $@"
+echo "All variables passed to script: $*"
+echo "Name of script: $0"
+echo "Number of variables passed: $3"
+echo "Curent working directory: $PWD"
+echo "Who is running: $USER"
+echo "Home directory of curent user: $HOME"
+echo "Hostname is: $HOSTNAME"
+echo "PID of script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
