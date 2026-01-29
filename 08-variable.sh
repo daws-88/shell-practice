e6c97359b59f834c40921313d134eca98b1c97f1
Venkat <psai96526@gmail.com>
2026-01-29 12:24:57 +0530

    up

--- 08-variable.sh
+++ 08-variable.sh
@@ -7,5 +7,6 @@ echo "presemt working directory: $PWD"
 echo "who is running: $USER"
 echo "HOME directory of user: $USER"
 echo "PID of script: "$$"
+echo "PID of this script is: $$"
 sleep 20 &
-echo "PID of last command in background: $!"
+echo "PID of last command in background is: $!"
