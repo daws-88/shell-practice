e0b91ff10cab42d08fb5d6ea72cb0a62db481278
Venkat <psai96526@gmail.com>
2026-01-29 12:21:14 +0530

    up

--- 08-variable.sh
+++ 08-variable.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "all variables passed to script is: $@"
-echo "all varaiables passed to script is: $*
-echo "name of script: $0"
-echo "curent directory: $PWD"
+echo "all varaiables passed to script is: $*"
+echo "name of script is: $0"
+echo "presemt working directory: $PWD"
 echo "who is running: $USER"
-echo "home directory of user: $HOME"
-echo "PID of script: $$"
-sleep 10 &
+echo "HOME directory of user: $USER"
+echo "PID of script: "$$"
+sleep 20 &
 echo "PID of last command in background: $!"
