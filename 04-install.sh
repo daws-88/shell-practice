50753db1ac0f7fe5167c561f2f24e54dfb72c3f9
Venkat <psai96526@gmail.com>
2026-01-30 18:36:18 +0530

    new

--- 04-install.sh
+++ 04-install.sh
@@ -3,11 +3,13 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: run this script with root privieelage"
+    exit 1
 fi
 
 dnf install nginx -y
 if [ $? -ne 0 ]; then
     echo "ERROR:: installing nginx is failure"
+    exit 1
 else
     echo "SUCCESS:: installing nginx is sucess"
 fi
