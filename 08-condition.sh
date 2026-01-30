df122b6728e449c2bc803c1e4b57513860cc2cd6
Venkat <psai96526@gmail.com>
2026-01-30 12:54:18 +0530

    up

---
+++ 08-condition.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -eq 0 ]; then
+    echo "rin this script"
+    exit 0
+else
+    echo "faliure"
+fi
+
+
+VALIDATE(){
+    if [ $1 -eq 0 ]; then
+        echo "SUCCESS:: installing $2 is success."
+        exit 0
+    else
+    echo "FAILURE:: installing $2 is failure."
+    fi
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+dnf install mysql -y
+VALIDATE $? "mysql"
+dnf install java -y
+VALIDATE $? "java"
