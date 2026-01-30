d3bded9f13e881fcf2a2415281ea5fba8cf7b522
Venkat <psai96526@gmail.com>
2026-01-30 19:04:55 +0530

    p

--- 02-condition.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "please enter number"
-
-read NUMBER
-if [ $(($NUMBER % 2)) -eq 0 ]; then
-    echo "given number $NUMBER is even"
-else
-    echo "please number $NUMBER is odd"
-fi
