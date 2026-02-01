8d1f2c4b429cac154bfa37a71535ef1ce071c3be
Venkat <psai96526@gmail.com>
2026-02-01 20:20:09 +0530

    new

--- 04-logs.sh
+++ 04-logs.sh
@@ -8,7 +8,7 @@ LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOG_FOLDER
-echo "Script strated at $(date)"
+echo "Script strated at $(date)" | tee -a $LOG_FILE
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script as root privelliage"
     exit 1
@@ -16,35 +16,35 @@ fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "installing $2 ....$R FAILURE $N"
+        echo -e "installing $2 ....$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else 
-        echo -e " installing $2....$G SUCCESS $N"
+        echo -e " installing $2....$G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
 
 
-dnf list installed nginx &>>$LOG_FILE
+dnf list installed nginx &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y &>>$LOG_FILE
+    dnf install nginx -y &>> $LOG_FILE
     VALIDATE $? "nginx"
 else
-    echo -e "Nginx is already exist....$Y SKIPPING $N"
+    echo -e "Nginx is already exist....$Y SKIPPING $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf list install mysql -y &>>$LOG_FILE
+    dnf list install mysql -y &>> $LOG_FILE
     VALIDATE $? "mysql"
 else
-    echo -e "Mysql is already exist...$Y SKIIPING $N"
+    echo -e "Mysql is already exist...$Y SKIIPING $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed nodejs &>>$LOG_FILE
+dnf list installed nodejs &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nodejs -y &>>$LOG_FILE
+    dnf install nodejs -y &>> $LOG_FILE
     VALIDATE $? "nodejs"
 else
-    echo -e "Nodejsis already exist....$Y Nodejs $N"
+    echo -e "Nodejsis already exist....$Y Nodejs $N" | tee -a $LOG_FILE
 fi
