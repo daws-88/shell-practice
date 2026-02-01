8b3ae1985f14567d9593561104f6c3b7daadda2d
Venkat <psai96526@gmail.com>
2026-02-01 20:31:21 +0530

    new

--- 04-logs.sh
+++ 04-logs.sh
@@ -25,9 +25,9 @@ VALIDATE() {
 
 
 
-dnf list installed nginx &>> $LOG_FILE
+dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y &>> $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "nginx"
 else
     echo -e "Nginx is already exist....$Y SKIPPING $N" | tee -a $LOG_FILE
@@ -35,15 +35,15 @@ fi
 
 dnf list installed mysql &>> $LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf list install mysql -y &>> $LOG_FILE
+    dnf list install mysql -y &>>$LOG_FILE
     VALIDATE $? "mysql"
 else
     echo -e "Mysql is already exist...$Y SKIIPING $N" | tee -a $LOG_FILE
 fi
 
-dnf list installed nodejs &>> $LOG_FILE
+dnf list installed nodejs &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nodejs -y &>> $LOG_FILE
+    dnf install nodejs -y &>>$LOG_FILE
     VALIDATE $? "nodejs"
 else
     echo -e "Nodejsis already exist....$Y Nodejs $N" | tee -a $LOG_FILE
