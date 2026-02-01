d9c4f7f26f3a7f9e82121bbcb1f43e139b56b73a
Venkat <psai96526@gmail.com>
2026-02-01 15:36:27 +0530

    new

--- 04-logs.sh
+++ 04-logs.sh
@@ -3,7 +3,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER='/var/log/shell-script"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOG_FOLDER
