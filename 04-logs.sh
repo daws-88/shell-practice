9178161921fff6307a81921b79817cfc981bb8e3
Venkat <psai96526@gmail.com>
2026-02-01 16:02:16 +0530

    new

--- 04-logs.sh
+++ 04-logs.sh
@@ -3,19 +3,16 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+USERID=$(id -u)
 LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOG_FOLDER
-echo "Script strated $(date")
-
-
-USERID=$(id -u)
+echo "Script strated at $(date)"
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script as root privelliage"
     exit 1
 fi
-
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
@@ -26,18 +23,28 @@ VALIDATE() {
     fi
 }
 
-dnf list installed nginx &>>LOG_FILE
+
+
+dnf list installed nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf install nginx -y &>>LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "nginx"
 else
     echo -e "Nginx is already exist....$Y SKIPPING $N"
 fi
 
-dnf list installed mysql &>>LOG_FILE
+dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    dnf list install mysql -y &>>LOG_FILE
+    dnf list install mysql -y &>>$LOG_FILE
     VALIDATE $? "mysql"
 else
     echo -e "Mysql is already exist...$Y SKIIPING $N"
+fi
+
+dnf list installed nodejs &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "nodejs"
+else
+    echo -e "Nodejsis already exist....$Y Nodejs $N"
 fi
