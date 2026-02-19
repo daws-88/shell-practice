9a4d7df86d47fa84cd9b1e48906437005709c36c
Venkat <psai96526@gmail.com>
2026-02-19 18:58:49 +0530

    new

--- 09-while.sh
+++ 09-while.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # whie loop is used to read file
-
+# | [ -n "$line" ]
 R="\e[31m"
-while  IFS= read -r line || [ -n "$line" ]
+while  IFS= read -r line 
 do
  echo -e "$R City: $line"
-done < name.sh
+done < 02-read.sh
