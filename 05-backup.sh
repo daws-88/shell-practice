a3f4f0c1ce652cc4f9e650e47c9e5b29bfd101db
Venkat <psai96526@gmail.com>
2026-02-23 16:33:56 +0530

    new

--- 05-backup.sh
+++ 05-backup.sh
@@ -1,26 +1,30 @@
+
 #!/bin/bash
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-
 SOURCE_DIR=$1
 DEST_DIR=$2
+DAYS=${3:-14} # if not provided considered as 14 days
 
-DAYS=${3:-14} # if not provided consider as 14days ##
-mkdir -p $LOG_FOLDER
-echo "Script strated at $(date)" | tee -a $LOG_FILE
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+#LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/backup.log" # modified to run the script as
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run this script as root privelliage"
-    exit 1
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-USAGE() {
-    echo -e "$R USAGE:: sudo  05-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS> $N" # DAYS optinonal
+USAGE(){
+    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days] $N"
     exit 1
 }
 
@@ -29,42 +33,46 @@ if [ $# -lt 2 ]; then
     USAGE
 fi
 
+### Check SOURCE_DIR Exist ####
 if [ ! -d $SOURCE_DIR ]; then
-    echo -e "$R Source directory $SOURCE_DIR does not exist $N"
+    echo -e "$R Source $SOURCE_DIR does not exist $N"
     exit 1
 fi
 
-if [ ! -d $DEST_DIR_DIR ]; then
-    echo -e "$R dest directory $SOURCE_DIR does not exist $N"
+### Check DEST_DIR Exist ####
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R Destination $DEST_DIR does not exist $N"
     exit 1
 fi
 
 ### Find the files ####
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
-if [ ! -z "${FILES}" ] ; then
+
+if [ ! -z "${FILES}" ]; then
+    ### Start Archeiving ###
     echo "Files found: $FILES"
     TIMESTAMP=$(date +%F-%H-%M)
-    ZIPFILE=$DEST_DIR/app-logs-$TIMESTAMP.zip
-    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIPFILE"
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    echo "Zip file name: $ZIP_FILE_NAME"
+    find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
 
-    ## check Archival sucess or not ###
-    if [ -f $ZIPFILE ];  then
-        echo -e "Archival..$G SUCCESS $N"
+    ### Check Archieval Success or not ###
+    if [ -f $ZIP_FILE_NAME ]
+    then
+        echo -e "Archeival ... $G SUCCESS $N"
 
-        while IFS= read -r project 
+        ### Delete if success ###
+        while IFS= read -r filepath
         do
-        echo "deleteing files $project
-        rm -rf $project
-        echo "Deleted files: $project
+            echo "Deleting the file: $filepath"
+            rm -rf $filepath
+            echo "Deleted the file: $filepath"
         done <<< $FILES
-    else 
-      
-        echo -e "Archival..$R FAILURE $N"
+    else
+        echo "Archieval ... $R FAILURE $N"
         exit 1
     fi
-    
 else
-    echo -e "NO files to Archieve $Y SKIIPING $N"
-    
+    echo -e "No files to archeive ... $Y SKIPPING $N"
 fi
