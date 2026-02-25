09fef9a5f764d18b2b47d7faf00f4497ab3be349
Venkat <psai96526@gmail.com>
2026-02-25 11:30:02 +0530

    new

---
+++ 03-disk-usage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 #in preoject we keep 75 percent
+IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+MESSAGE=""
+while IFS= read -r line
+do 
+    USAGE=$(echo $line | awk '{print $6}'| cut -d "%" -f1)
+    PARTION=$(echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High usage on $PARTION: $USAGE% <br>"
+    fi
+    
+done <<< $DISK_USAGE
+echo -e "Message Body: $MESSAGE"
+
+sh mail.sh "psai96526@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "Devops Team"
+
+
