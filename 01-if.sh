ac5997820954dfbfadb70f7d03aae81d2a57bd20
Venkat <psai96526@gmail.com>
2026-04-18 10:44:01 +0530

    imp

--- 01-if.sh
+++ 01-if.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Enter Your Number"
 read NUMBER
-if [ $((NUMBER % 2)) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given Number $NUMBER is even"
 else
     echo "Given Number $NUMBER is odd"
 fi
- 
+ if [ $(($NUMBER % 2)) -eq 0 ]; then
