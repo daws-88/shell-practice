0e67595c222d4ce49985aa50e9ceb1f952db8faa
Venkat <psai96526@gmail.com>
2026-04-18 09:21:23 +0530

    imp

---
+++ 01-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=sanju
+PERSON12=dhoni
+
+echo "$PERSON1:: hello $PERSON2?"
+echo "$PERSON2:: i am fine $PERSON1"
+echo "$PERSON1:: shall we play cricket $PERSON2?"
+echo "$PERSON2:: yes $PERSON1"
---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# argument passing
+PERSON1=$1
+PERSON12=$2
+
+echo "$PERSON1:: hello $PERSON2?"
+echo "$PERSON2:: i am fine $PERSON1"
+echo "$PERSON1:: shall we play cricket $PERSON2?"
+echo "$PERSON2:: yes $PERSON1"
