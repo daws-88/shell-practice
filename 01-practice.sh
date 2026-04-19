065bd5ed1ebe2bdb7e7cc1503fa0d0064e01ecf8
Venkat <psai96526@gmail.com>
2026-04-19 17:12:31 +0530

    imp

--- 01-practice.sh
+++ 01-practice.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-# want to print numbers 1-10
-x=1 # we are intalizing
-while [ $x -le 10 ]
+# whie loop is used to read file
+R="\e[31m"
+while IFS= read -r filepath || [ -n "$filepath" ]
 do
-    echo "$x"
-    sleep 2 
-    x=$((x+1))
-done  
-
-
-# #!/bin/bash
-# # whie loop is used to read file
-
-# R="\e[31m"
-# while IFS= read -r filepath || [ -n "$filepath" ]
-# do
-#   echo -e " $R city: $filepatg $N"
-# done < name .sh 
+  echo -e " $R city: $filepatg $N"
+done < name .sh 
 
