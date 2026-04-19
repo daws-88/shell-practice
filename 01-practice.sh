7e6553de17310d88bb15c6baf54a4fbfd33612aa
Venkat <psai96526@gmail.com>
2026-04-19 17:08:18 +0530

    imp

--- 01-practice.sh
+++ 01-practice.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-R="\e[31m"
-N="\e[0m"
-COUNTRY=INDIA
-echo -e "my country name is:$R $COUNTRY $N"
-echo "PID of script is: $$"
-source ./02-script2.sh
+# want to print numbers 1-10
+x=1 # we are intalizing
+while [ $x -le 10 ]; then
+do
+    echo "$x"
+    sleep 2 
+    x=$((x+1))
+done  
+
+
+# #!/bin/bash
+# # whie loop is used to read file
+
+# R="\e[31m"
+# while IFS= read -r filepath || [ -n "$filepath" ]
+# do
+#   echo -e " $R city: $filepatg $N"
+# done < name .sh 
 
