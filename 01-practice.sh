d2e7cca9c4a6ea263220bc4b0c7e99e3376ffc10
Venkat <psai96526@gmail.com>
2026-04-20 17:09:31 +0530

    imp

--- 01-practice.sh
+++ 01-practice.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-my_list=("apple" "banana" "cherry")
-my_string="${my_list[*]}"
-echo "$my_string"
+# Script to print a pyramid of 5 stars (rows)
+
+rows=5
+for ((i=1; i<=rows; i++))
+do
+  # Inner loop to print leading spaces for centering
+  for ((j=i; j<rows; j++))
+  do
+    echo -n " "
+  done
+
+  # Inner loop to print stars (using 2*i-1 logic for odd growth)
+  for ((k=1; k<=(2*i-1); k++))
+  do
+    echo -n "*"
+  done
+
+  # Move to the next line after each row
+  echo ""
+done
